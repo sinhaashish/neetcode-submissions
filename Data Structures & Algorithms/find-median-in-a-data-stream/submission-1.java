f3798cd08db149aae6d716be99b342e37f319b64
Ashish Kumar Sinha <ashi.sinha.87@gmail.com>
2026-06-16 14:13:11 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.java
@@ -0,0 +1,33 @@
+class MedianFinder {
+
+    private PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> maxHeap;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>();
+
+        maxHeap = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b, a)
+        );
+    }
+
+    public void addNum(int num) {
+
+        maxHeap.offer(num);
+
+        minHeap.offer(maxHeap.poll());
+
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+
+        if (maxHeap.size() > minHeap.size()) {
+            return maxHeap.peek();
+        }
+
+        return ((double) maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+}

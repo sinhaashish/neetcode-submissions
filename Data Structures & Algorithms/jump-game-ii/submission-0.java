a522b8f12a20680b79915e784eee28a42a19a64c
Ashish Kumar Sinha <ashi.sinha.87@gmail.com>
2026-06-20 18:39:31 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int jump(int[] nums) {
+        int end =0 ; int farthest = 0; int jumps =0 ; 
+        for( int i =0 ; i < nums.length -1 ; i++) {
+            farthest = Math.max( farthest, i + nums[i]);
+            if ( i == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+}

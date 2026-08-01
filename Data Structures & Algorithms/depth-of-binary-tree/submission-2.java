e58f4734ede92acb894910c3f9057a7614338fd4
Ashish Kumar Sinha <ashi.sinha.87@gmail.com>
2026-08-01 12:21:12 +0530

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+       
+        return  root == null ? 0 : 
+        1 + Math.max(maxDepth(root.left) , maxDepth(root.right));
+    
+    }
+}
